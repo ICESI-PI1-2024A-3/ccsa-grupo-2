626e072bde0af43b39ce5f87659a34dd27b5e74b
ShammerY <samuel.ibarra1227@gmail.com>
2024-03-17 19:24:39 -0500

    add charge request model and change in forms

--- requests_mgmt/models.py
+++ requests_mgmt/models.py
@@ -14,6 +14,23 @@ class RequestType(models.Model):
     type = models.CharField(max_length=100)
 
 
+class ChargeAccountRequest(models.Model):
+    user_name = models.CharField(default='Not Set',max_length=20)
+    document_type = models.CharField(default='Not Set')
+    user_id = models.CharField(max_length=30)
+    amount = models.FloatField(default=0)
+    concept = models.CharField(max_length=200)
+    costs_and_deductions = models.BooleanField(default=True)
+    rent_tax_declarant = models.BooleanField(default=True)
+    fiscal_resident = models.BooleanField(default=False)
+    city = models.CharField(default='Not Set')
+    date = models.DateField(default='Not Set')
+    bank_name = models.CharField(default='Not Set')
+    account_type = models.CharField(default='Not Set')
+    account_number = models.CharField(default='Not Set')
+    CEX_no = models.CharField(default='Not Set')
+
+
 class Request(models.Model):
     type = models.ForeignKey(RequestType, on_delete=models.CASCADE)
     status = models.ForeignKey(RequestStatus, on_delete=models.CASCADE)
--- requests_mgmt/views.py
+++ requests_mgmt/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
+class RequestViews:
+    def createRequest(request):
+        if request.method=='GET':
+            return render(request,'createRequest.html',{
+                'userInfoForm':user_information(),
+                'inputForm':CreateNewChargeAccount()
+            })
+        
+        
+
