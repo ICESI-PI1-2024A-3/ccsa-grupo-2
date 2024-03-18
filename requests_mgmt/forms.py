3065287042a21ab17c4d06cee6d899b7637de4dd
MalteDs <dmalte2002@hotmail.com>
2024-03-18 00:33:36 -0500

    feat: add new collection account request fields

--- requests_mgmt/forms.py
+++ requests_mgmt/forms.py
@@ -11,7 +11,7 @@ class user_information(forms.Form):
     user_name = forms.CharField(label="Nombre de solicitante:", max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     user_id = forms.CharField(label="Identificación solicitante (C.C.):", max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    
+
     document_type = forms.ChoiceField(label="Tipo de documento:", choices=DOCUMENT_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
     
     
@@ -20,10 +20,31 @@ class user_information(forms.Form):
 
 class CreateNewChargeAccount(forms.Form):
 
-    amount = forms.DecimalField(label="$", max_digits=10, decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    amount = forms.DecimalField(label="Monto $", max_digits=10, decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
     concept = forms.CharField(label="Concepto:", widget=forms.Textarea(attrs={'class': 'form-control'}))
 
+class TaxTreatmentForm(forms.Form):
+    CHOICES = [
+        ('392_401', 'Retención en la fuente artículos 392 y 401 del Estatuto Tributario'),
+        ('383', 'Retención en la fuente artículo 383 del Estatuto Tributario'),
+    ]
+
+    checkbox_choices = forms.ChoiceField(
+        label="Se tomarán costos y deducciones asociados a las rentas de trabajo por los servicios prestados:",
+        choices=CHOICES,
+        widget=forms.RadioSelect,  # Utilizamos RadioSelect para permitir solo una selección
+    )
+
+class CheckboxRentaResidente(forms.Form):
+    declarante = forms.BooleanField(label="Soy declarante de impuesto de renta", required=False)
+    
+    residente = forms.BooleanField(label="Soy residente fiscal en Colombia", required=False)
+
+class City_Date(forms.Form):
+    city = forms.CharField(label = "Ciuadad: ", max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    date = forms.DateField(label="Fecha:", widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
 
 
     
--- requests_mgmt/views.py
+++ requests_mgmt/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
-from .forms import user_information, CreateNewChargeAccount
+from .forms import user_information, CreateNewChargeAccount, TaxTreatmentForm, CheckboxRentaResidente,  City_Date
 
 # Create your views here.
 
@@ -11,5 +11,8 @@ def request_index(request):
 def charge_account(request):
     return render(request, "request_type/charge_account.html", {
         "user_form": user_information(),
-        "charge_account": CreateNewChargeAccount()
+        "charge_account": CreateNewChargeAccount(),
+        "taxTreatment": TaxTreatmentForm,
+        "checkBox": CheckboxRentaResidente,
+        "city_date": City_Date
     })
