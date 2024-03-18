a4953269297c6a8d19bcd6ac8e0beff9a9d84681
MalteDs <dmalte2002@hotmail.com>
2024-03-18 15:38:34 -0500

    feat: rename base html to fix errors

--- requests_mgmt/urls.py
+++ requests_mgmt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_request/', views.request_index, name = 'request_index'),
-    path("new_request/charge_account/", views.charge_account, name = "charge_account" )
+    path('new_request/', views.RequestViews.request_index, name = 'request_index'),
+    path("new_request/charge_account/", views.RequestViews.charge_account, name = "charge_account" )
 ]
