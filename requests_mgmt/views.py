dad50dc2595d9f3024e44d66f15ae53265b85196
ShammerY <samuel.ibarra1227@gmail.com>
2024-03-17 20:03:34 -0500

    update request views and implement charge request creation

--- requests_mgmt/views.py
+++ requests_mgmt/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
+from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 class RequestViews:
@@ -9,6 +11,41 @@ class RequestViews:
                 'userInfoForm':user_information(),
                 'inputForm':CreateNewChargeAccount()
             })
-        
-        
+        else:
+            try:
+                chargeRequest = ChargeAccountRequest.objects.create(
+                user_name = request.POST['user_name'],
+                user_id=request.POST['user_id'],
+                document_type=request.POST['document_type'],
+                amount=request.POST['amount'],
+                concept=request.POST['concept'],
+                city=request.POST['city'],
+                date=request.POST['date'],
+                bank_name=request.POST['bank_name'],
+                account_type=request.POST['account_type'],
+                account_number=request.POST['account_number'],
+                CEX_no=request.POST['CEX_no']
+            )
+                costs_and_deductions = request.POST.get('costs_and_deductions',False)
+                rent_tax_declarant = request.POST.get('rent_tax_declarant',False)
+                fiscal_resident = request.POST.get('fiscal_resident',False)
+
+                if costs_and_deductions:
+                    chargeRequest.isCost_and_Deductions(True)
+                else:
+                    chargeRequest.isCost_and_Deductions(True)
+                
+                if rent_tax_declarant:
+                    chargeRequest.isRent_Tax_Declarant(True)
+                else:
+                    chargeRequest.isRent_Tax_Declarant(True)
+                
+                if fiscal_resident:
+                    chargeRequest.isFiscal_Resident(True)
+                else:
+                    chargeRequest.isFiscal_Resident(True)
+                return redirect('home')
+            except:
+                return HttpResponse('An Error Has Ocurred')
+            
 
