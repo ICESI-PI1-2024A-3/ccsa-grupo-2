2f761a8be0b829d0173771fafc661d28abc22e8c
lolito996 <josealejandromc4@gmail.com>
2024-03-23 21:05:44 -0500

    refactor: changes in views forms request details

--- requests_mgmt/urls.py
+++ requests_mgmt/urls.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import include, path
-
-from requests_mgmt.views import RequestViews
-
-urlpatterns = [
-    path('show_requests/',RequestViews.showRequests,name='show_requests'),
-    path('edit_request/<int:request_id>',RequestViews.edit_request,name='edit_request'),
-    path('delete_request/<int:request_id>',RequestViews.delete_request,name='delete_request'),
-    path('new_request/', RequestViews.request_index, name = 'request_index'),
-    path("new_request/charge_account/", RequestViews.charge_account, name = "charge_account"),
-    path("show_requests/request_details/<int:request_id>", RequestViews.detail_request, name='details')
-
-]
-
-
-=======
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
@@ -27,6 +9,7 @@ from .views import (
     HomeView,
     RequestsListView,
     RequestsView,
+    UpdateRequestView,
 )
 
 urlpatterns = [
@@ -36,6 +19,7 @@ urlpatterns = [
         login_required(DetailsRequestView.as_view()),
         name="detail_request",
     ),
+    path("update/<int:request_id>", login_required(UpdateRequestView.as_view()),name="update_request"),
     path(
         "<int:request_id>/editar",
         login_required(EditRequestView.as_view()),
@@ -53,4 +37,3 @@ urlpatterns = [
         name="charge_account",
     ),
 ]
->>>>>>> dev
