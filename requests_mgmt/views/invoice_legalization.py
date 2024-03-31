1b6bac37a95e658b48ee5a1ed617ec70a866f9f0
ShammerY <samuel.ibarra1227@gmail.com>
2024-03-31 18:40:41 -0500

    feat: data capture of invoice legalization format

--- requests_mgmt/views/invoice_legalization.py
+++ requests_mgmt/views/invoice_legalization.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
 from django.views import View
 
 from ..forms import (
@@ -7,7 +7,7 @@ from ..forms import (
     BalanceDiscountAutorizationForm,
     BankInformation
 )
-# from ..models import 
+from ..models import InvoiceLegalizationRequest,RequestStatus
 
 class InvoiceLegalizationView(View):
     template_name = "requests/create_invoice_legalization_request.html"
@@ -24,3 +24,37 @@ class InvoiceLegalizationView(View):
             "balance_discount_autorization_form": self.balance_discount_autorization_form,
             "bank_information": self.bank_information
         })
+    
+    def post(self, request, *args, **kwargs):
+        try:
+            legalization_date = request.POST.get("legalization_date")
+            dependency = request.POST.get("dependency")
+            destination_city = request.POST.get("destination_city")
+            departure_date = request.POST.get("departure_date")
+            reason_trip = request.POST.get("reason_trip")
+            discount_authorization = request.POST.get("autorizar_descuento")
+            bank_name = request.POST.get("bank_name")
+            account_type = request.POST.get("account_type")
+            account_number = request.POST.get("account_number")
+            requester = request.user
+            invoice_legalization_request = InvoiceLegalizationRequest.objects.create(
+                requester = requester,
+                type = 'Legalización de Factura',
+                status = RequestStatus.objects.get(id=1),
+                legalization_date = legalization_date,
+                dependency = dependency,
+                destination_city = destination_city,
+                departure_date = departure_date,
+                reason_trip = reason_trip,
+                discount_authorization = discount_authorization,
+                bank_name = bank_name,
+                account_type = account_type,
+                account_number = account_number
+
+            )
+
+            invoice_legalization_request.save()
+
+            return redirect("requests_list")
+        except ValueError:
+            return HttpResponse("An Error Has Ocurred")  ## CAMBIAR ESTO POR REDIRECCION:
