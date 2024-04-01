1269d9d50c333ba3334ce9446f2af94cba4b10fc
lolito996 <josealejandromc4@gmail.com>
2024-04-01 12:51:21 -0500

    feat: improve the html

--- requests_mgmt/views/update_approver.py
+++ requests_mgmt/views/update_approver.py
@@ -6,12 +6,20 @@ from django.shortcuts import redirect
 
 class UpdateApproverView(View):
 
-    def post(self, request,request_id, *args, **kwargs):
+    def post(self, request, request_id, *args, **kwargs):
         user_id = request.POST.get('Aprovadores')
-        intance_request = Request.objects.get(pk=request_id)
-        if user_id is None or intance_request is None:
+        if user_id is None or user_id == '':
             return redirect('detail_request', request_id=request_id)
-        user = CustomUser.objects.get(pk=user_id)
-        intance_request.approver = user
-        intance_request.save()
-        return redirect('detail_request', request_id=request_id)
+
+        try:
+            # Si se ha seleccionado un aprobador válido, actualiza la solicitud.
+            instance_request = Request.objects.get(pk=request_id)
+            user = CustomUser.objects.get(pk=user_id)
+            instance_request.approver = user
+            instance_request.save()
+            return redirect('detail_request', request_id=request_id)
+        except (Request.DoesNotExist, CustomUser.DoesNotExist) as e:
+            instance_request = Request.objects.get(pk=request_id)
+            instance_request.approver = None
+            instance_request.save()
+            return redirect('detail_request', request_id=request_id)
--- requests_mgmt/views/update_reviewer.py
+++ requests_mgmt/views/update_reviewer.py
@@ -7,12 +7,20 @@ from django.shortcuts import redirect
 
 class UpdateReviewerView(View):
 
-    def post(self, request,request_id, *args, **kwargs):
+    def post(self, request, request_id, *args, **kwargs):
         user_id = request.POST.get('Revisores')
-        intance_request = Request.objects.get(pk=request_id)
-        if user_id is None or intance_request is None:
+        if user_id is None or user_id == '':
             return redirect('detail_request', request_id=request_id)
-        user = CustomUser.objects.get(pk=user_id)
-        intance_request.reviewer = user
-        intance_request.save()
-        return redirect('detail_request', request_id=request_id)
+
+        try:
+
+            instance_request = Request.objects.get(pk=request_id)
+            user = CustomUser.objects.get(pk=user_id)
+            instance_request.reviewer = user
+            instance_request.save()
+            return redirect('detail_request', request_id=request_id)
+        except (Request.DoesNotExist, CustomUser.DoesNotExist) as e:
+            instance_request = Request.objects.get(pk=request_id)
+            instance_request.reviewer = None
+            instance_request.save()
+            return redirect('detail_request', request_id=request_id)
