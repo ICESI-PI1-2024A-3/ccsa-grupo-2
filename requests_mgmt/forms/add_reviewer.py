cddc1377116d165521463dc52c7d468ebf5b1591
lolito996 <josealejandromc4@gmail.com>
2024-04-13 15:13:19 -0500

    Feat: add html new request

--- requests_mgmt/forms/add_reviewer.py
+++ requests_mgmt/forms/add_reviewer.py
@@ -6,11 +6,11 @@ from users_mgmt.models import CustomUser
 class AddReviewerForm(forms.Form):
     request_id = forms.IntegerField(widget=forms.HiddenInput())
     type = forms.CharField(widget=forms.HiddenInput())
-    Revisores = forms.ChoiceField(required=False)
+    reviewers = forms.ChoiceField(required=False,label='Revisores')
 
     def __init__(self, *args, **kwargs):
         super(AddReviewerForm, self).__init__(*args, **kwargs)
-        Revisores = CustomUser.objects.filter(role="Reviewer")
-        reviewer_choices = [(reviewer.id, reviewer) for reviewer in Revisores]
+        reviewers = CustomUser.objects.filter(role="Reviewer")
+        reviewer_choices = [(reviewer.id, reviewer) for reviewer in reviewers]
         reviewer_choices.insert(0,('', '< selecione un revisor >'))
-        self.fields['Revisores'].choices = reviewer_choices
+        self.fields['reviewers'].choices = reviewer_choices
