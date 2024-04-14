bee902bd6b282bbc8e96f9ed457d2509d556a310
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-04-14 18:37:58 -0500

    feat: adjust layout in the requests details template

--- requests_mgmt/forms/add_approver.py
+++ requests_mgmt/forms/add_approver.py
@@ -6,11 +6,11 @@ from users_mgmt.models import CustomUser
 class AddApproverForm(forms.Form):
     request_id = forms.IntegerField(widget=forms.HiddenInput())
     type = forms.CharField(widget=forms.HiddenInput())
-    approvers = forms.ChoiceField(required=False, label='Aprovadores')  #
+    approvers = forms.ChoiceField(required=False, label="")
 
     def __init__(self, *args, **kwargs):
         super(AddApproverForm, self).__init__(*args, **kwargs)
         approvers = CustomUser.objects.filter(role="Approver")
         approver_choices = [(approver.id, approver) for approver in approvers]
-        approver_choices.insert(0,('', '< seleccione un aprovador >'))
-        self.fields['approvers'].choices = approver_choices
+        approver_choices.insert(0, ("", "< seleccione un aprovador >"))
+        self.fields["approvers"].choices = approver_choices
