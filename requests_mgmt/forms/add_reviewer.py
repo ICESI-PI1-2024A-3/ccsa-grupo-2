81cebc19a055c57cff70c98507ee8b80a59e1a66
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-04-15 16:28:25 -0500

    fix: fix bug in reviewer and approver choice field

--- requests_mgmt/forms/add_reviewer.py
+++ requests_mgmt/forms/add_reviewer.py
@@ -4,13 +4,18 @@ from users_mgmt.models import CustomUser
 
 
 class AddReviewerForm(forms.Form):
-    request_id = forms.IntegerField(widget=forms.HiddenInput())
-    type = forms.CharField(widget=forms.HiddenInput())
+    # request_id = forms.IntegerField(widget=forms.HiddenInput())
+    # type = forms.CharField(widget=forms.HiddenInput())
     reviewers = forms.ChoiceField(required=False, label="")
 
     def __init__(self, *args, **kwargs):
+        selected_reviewer_id = kwargs.pop("selected_reviewer_id", None)
         super(AddReviewerForm, self).__init__(*args, **kwargs)
         reviewers = CustomUser.objects.filter(role="Reviewer")
-        reviewer_choices = [(reviewer.id, reviewer) for reviewer in reviewers]
-        reviewer_choices.insert(0, ("", "< selecione un revisor >"))
+        reviewer_choices = [(0, "Selecione un revisor")] + [
+            (reviewer.id, reviewer) for reviewer in reviewers
+        ]
         self.fields["reviewers"].choices = reviewer_choices
+
+        if selected_reviewer_id:
+            self.fields["reviewers"].initial = selected_reviewer_id
