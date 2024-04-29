9b5214acd655e4dc5ce24c78af35105d30bb1ed5
Osejo <alejandrosejo@outlook.com>
2024-04-28 22:27:51 -0500

    feat: add the funtions of the bottons and make corrections about the filters

--- users_mgmt/views/approve_as_approver.py
+++ users_mgmt/views/approve_as_approver.py
@@ -19,35 +19,35 @@ class ApproveAsApproverView(View):
             {"requests_to_approval": requests_to_approval}
         )
 
-    def post(self, request):
-        user_id = request.POST.get("user_id")
-        action = request.POST.get("action")  # Aquí se podría definir algún campo en el formulario para la acción, como un botón de "aprobar"
+    # def post(self, request):
+    #     user_id = request.POST.get("user_id")
+    #     action = request.POST.get("action")  # Aquí se podría definir algún campo en el formulario para la acción, como un botón de "aprobar"
 
-        try:
-            user = User.objects.get(pk=user_id)
-            if action == "approve":
-                # Lógica para aprobar al usuario
-                user.approved = True
-                user.save()
-                message = "Usuario aprobado correctamente"
-            elif action == "reject":
-                # Lógica para rechazar al usuario
-                user.approved = False
-                user.save()
-                message = "Usuario rechazado correctamente"
-            else:
-                message = "Acción no válida"
+    #     try:
+    #         user = User.objects.get(pk=user_id)
+    #         if action == "approve":
+    #             # Lógica para aprobar al usuario
+    #             user.approved = True
+    #             user.save()
+    #             message = "Usuario aprobado correctamente"
+    #         elif action == "reject":
+    #             # Lógica para rechazar al usuario
+    #             user.approved = False
+    #             user.save()
+    #             message = "Usuario rechazado correctamente"
+    #         else:
+    #             message = "Acción no válida"
 
-            # Obtener todos los usuarios que necesitan aprobación después de la acción realizada
-            users_needing_approval = User.objects.filter(needs_approval=True)
-            return render(
-                request,
-                self.template_name,
-                {"users_needing_approval": users_needing_approval, "success": message},
-            )
-        except User.DoesNotExist:
-            return render(
-                request,
-                self.template_name,
-                {"error": "Usuario no encontrado"},
-            )
+    #         # Obtener todos los usuarios que necesitan aprobación después de la acción realizada
+    #         users_needing_approval = User.objects.filter(needs_approval=True)
+    #         return render(
+    #             request,
+    #             self.template_name,
+    #             {"users_needing_approval": users_needing_approval, "success": message},
+    #         )
+    #     except User.DoesNotExist:
+    #         return render(
+    #             request,
+    #             self.template_name,
+    #             {"error": "Usuario no encontrado"},
+    #         )
