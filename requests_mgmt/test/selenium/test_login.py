e52653c621505435e060200c635472797c0cb571
ShammerY <samuel.ibarra1227@gmail.com>
2024-04-30 00:36:21 -0500

    feat: create user registration test with selenium

--- requests_mgmt/test/selenium/test_login.py
+++ requests_mgmt/test/selenium/test_login.py
@@ -2,7 +2,6 @@ import unittest
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-##from . import constants as const
 
 class Login(StaticLiveServerTestCase):
     def setUp(self):
@@ -15,15 +14,14 @@ class Login(StaticLiveServerTestCase):
         self.driver.quit()
 
     def test_login(self):
-        print("Inicia el Test")
         user = self.driver.find_element(By.NAME, "username")
         user.send_keys("123")
         password = self.driver.find_element(By.NAME, "password")
         password.send_keys("123")
         loginButton = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div/div/form/div[3]/div[1]/button")
         loginButton.click()
-        text_expected = self.driver.find_element(By.XPATH, '/html/body/section/div[1]')
-        self.assertEqual(text_expected.text, "Apoyo Logistico")
+        text_expected = self.driver.find_element(By.NAME, 'welcome')
+        self.assertEqual(text_expected.text, "Bienvenido")
 
 if __name__ == "_main_":
     unittest.main()
