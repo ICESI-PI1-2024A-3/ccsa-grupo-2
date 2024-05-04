dab882fe6108f27c97e94d5f2328b48e1f5e17e0
lolito996 <josealejandromc4@gmail.com>
2024-05-04 17:05:57 -0500

    feat:add model dialog for request details

--- accounting_management/settings.py
+++ accounting_management/settings.py
@@ -91,26 +91,26 @@ DB_PORT = os.getenv('DB_PORT')
 
 #Conexion con Postgres
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT,
-    }
-}
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': DB_NAME,
+#        'USER': DB_USER,
+#        'PASSWORD': DB_PASSWORD,
+#        'HOST': DB_HOST,
+#        'PORT': DB_PORT,
+#    }
+#}
 
 
 #Conexion con Sqlite
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
+     }
+}
 
 
 # Password validation
