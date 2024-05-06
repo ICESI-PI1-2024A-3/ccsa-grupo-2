4b9afc496f6a2757f6c3d6e58ee093764e995bd3
Osejo <alejandrosejo@outlook.com>
2024-05-06 14:02:04 -0500

    feat: mka ecorrections in template of approve_as_approver and test of approve_as_reviewer

--- users_mgmt/tests/selenium/test_approve_as_reviewer.py
+++ users_mgmt/tests/selenium/test_approve_as_reviewer.py
@@ -1,9 +1,39 @@
+from telnetlib import EC
 import unittest
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
+from selenium.webdriver.common.keys import Keys
+
+class TestApprovalProcess(StaticLiveServerTestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://127.0.0.1:8000/")
+        self.driver.implicitlywait(15)
+        self.driver.maximizewindow()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_approval_page(self):
+        # Navegar a la página de solicitudes por revisar
+        self.driver.get("http://127.0.0.1:8000/approve_as_reviewer/")  
+
+        # Esperar a que la página se cargue completamente
+        WebDriverWait(self.driver, 10).until( # type: ignore
+            EC.presence_of_element_located((By.CLASS_NAME, "custom-card"))
+        )
+
+        # Verificar que existan elementos en la página
+        requests_elements = self.driver.find_elements_by_class_name("custom-card")
+        self.assertTrue(len(requests_elements) > 0)  # Verificar que haya al menos una solicitud por revisar
+
+if __name__ == "__main__":
+    unittest.main()
+        
 class ApproveAsReviewer(StaticLiveServerTestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
