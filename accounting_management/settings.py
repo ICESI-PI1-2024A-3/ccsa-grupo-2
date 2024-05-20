9c70a5d088ac490d926ef795fd4d2b6aecc438b4
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-05-19 22:56:50 -0500

    chore: Revert changes to static file serving configuration

--- accounting_management/settings.py
+++ accounting_management/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 # if not DEBUG:
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
