7232a8a438c549225eb7eea8b98fe07dc2c6cde9
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-05-19 22:58:36 -0500

    chore: Enable static file serving with whitenoise middleware

--- accounting_management/settings.py
+++ accounting_management/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 # if not DEBUG:
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
